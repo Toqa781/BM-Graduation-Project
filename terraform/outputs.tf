7b39e7dc2d0ba5920dd2e5fda8f83c533a28e77d
mohamedashrafz835 <mohamed.ashraf65@msa.edu.eg>
2025-08-29 15:05:48 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,44 +1,45 @@
 output "eks_cluster_name" {
   description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.eks.name
+  value       = aws_eks_cluster.stage_eks.name
 }
 
 output "eks_cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = aws_eks_cluster.eks.endpoint
+  value       = aws_eks_cluster.stage_eks.endpoint
 }
 
 output "eks_cluster_version" {
   description = "The Kubernetes version for the EKS cluster"
-  value       = aws_eks_cluster.eks.version
+  value       = aws_eks_cluster.stage_eks.version
 }
 
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value       = aws_vpc.the_vpc.id
 }
 
 output "cluster_id" {
   description = "The ID of the EKS cluster"
-  value       = aws_eks_cluster.eks.id
+  value       = aws_eks_cluster.stage_eks.id
 }
 
 output "node_group_id" {
   description = "EKS node group ID"
-  value       = aws_eks_node_group.eks_nodes.id
+  value       = aws_eks_node_group.nodes_general.id
 }
 
 output "subnet_ids" {
   description = "List of IDs of the subnets"
-  value       = [aws_subnet.public[*].id, aws_subnet.private[*].id]
+  value       = aws_subnet.the_subnet[*].id
 }
 
 output "cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
-  value       = aws_security_group.eks_sg.id
+  value       = aws_security_group.allow_tls.id
 }
 
 output "region" {
   description = "AWS region"
   value       = var.region
 }
+

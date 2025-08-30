e76d1af45ec0fa855c95202568cebad341d1ecef
mohamedashrafz835 <mohamed.ashraf65@msa.edu.eg>
2025-08-30 18:11:41 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,31 +1,16 @@
-output "eks_cluster_name" {
-  value = aws_eks_cluster.stage_eks.name
-}
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.stage_eks.endpoint
-}
-
 output "vpc_id" {
-  value = aws_vpc.stage_vpc.id
+  description = "VPC ID"
+  value = module.vpc.vpc_id
 }
 
 
-output "cluster_id" {
-  value = aws_eks_cluster.stage_eks.id
-}
-
-output "node_group_id" {
-  value = aws_eks_node_group.stage_eks_node_group.id
+output "cluster_name" {
+  description = "EKS cluster name"
+  value = module.eks.cluster_name
 }
 
 
-output "subnet_ids" {
-  value = aws_subnet.stage_subnet[*].id
-}
-
-
-output "cluster_token" {
-  value = data.aws_eks_cluster_auth.stage_eks.token
-  sensitive = true
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value = module.eks.cluster_endpoint
 }

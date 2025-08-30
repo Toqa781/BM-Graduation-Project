75fc05c728cb1efeade6f077b9e2f48aa085c290
mohamedashrafz835 <mohamed.ashraf65@msa.edu.eg>
2025-08-30 14:05:45 +0300

    changing to the right region

--- terraform/1-backend.tf
+++ terraform/1-backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket="statefile-bucket-bmfinal"
     key="./terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-west-2"
     dynamodb_table = "statefile-table"
   }
 }

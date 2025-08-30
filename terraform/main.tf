33840175ec1bafabf5c2753f5449019dbf1064f1
mohamedashrafz835 <mohamed.ashraf65@msa.edu.eg>
2025-08-30 10:19:07 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "stagebucket12"
-    key            = "stage-eks1/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
+
 
 provider "aws" {
   region = var.region
